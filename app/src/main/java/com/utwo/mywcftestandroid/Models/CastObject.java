e7b6a17842f942ca13b442ffaa98d9100e37e520
zhuxiang1212 <zhuxiang1212@hotmail.com>
2015-10-22 17:20:39 +0800

    Commit

--- app/src/main/java/com/utwo/mywcftestandroid/Models/CastObject.java
+++ app/src/main/java/com/utwo/mywcftestandroid/Models/CastObject.java
@@ -4,6 +4,8 @@ import org.ksoap2.serialization.KvmSerializable;
 import org.ksoap2.serialization.PropertyInfo;
 import org.ksoap2.serialization.SoapObject;
 
+import java.util.Date;
+
 /**
  * Created by ammic on 2015/10/22.
  */
@@ -21,9 +23,17 @@ public class CastObject {
             //处理property不存在的情况
             try {
                 if (info.getType() == PropertyInfo.STRING_CLASS) {
-                    result.setProperty(i, soapObject.getProperty(info.name).toString());
+                    result.setProperty(i, soapObject.getPropertySafelyAsString(info.name));
                 } else if (info.getType() == PropertyInfo.INTEGER_CLASS) {
-                    result.setProperty(i, Integer.parseInt(soapObject.getProperty(info.name).toString()));
+                    result.setProperty(i, Integer.parseInt(soapObject.getPropertySafelyAsString(info.name)));
+                } else if (info.getType() == PropertyInfo.LONG_CLASS) {
+                    result.setProperty(i, Long.parseLong(soapObject.getPropertySafelyAsString(info.name)));
+                } else if (info.getType() == PropertyInfo.BOOLEAN_CLASS) {
+                    result.setProperty(i, Boolean.parseBoolean(soapObject.getPropertySafelyAsString(info.name)));
+                } else if (info.getType() == Float.class) {
+                    result.setProperty(i, Float.parseFloat(soapObject.getPropertySafelyAsString(info.name)));
+                } else if (info.getType() == Date.class) {
+                    //result.setProperty(i, Date.parseFloat(soapObject.getProperty(info.name).toString()));
                 }
             } catch (Exception e) {
                 continue;
--- app/src/main/java/com/utwo/mywcftestandroid/Models/Product.java
+++ app/src/main/java/com/utwo/mywcftestandroid/Models/Product.java
@@ -2,6 +2,7 @@ package com.utwo.mywcftestandroid.Models;
 
 import org.ksoap2.serialization.PropertyInfo;
 
+import java.util.Date;
 import java.util.Hashtable;
 
 /**
@@ -12,6 +13,9 @@ public class Product extends BaseModel {
 
     private int ProductID;
     private String ProductName;
+    private float UnitPrice;
+    private boolean IsSoldOut;
+    private Date SoldOutDate;
 
     public int getProductID() {
         return ProductID;
@@ -29,6 +33,30 @@ public class Product extends BaseModel {
         ProductName = productName;
     }
 
+    public float getUnitPrice() {
+        return UnitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        UnitPrice = unitPrice;
+    }
+
+    public boolean isSoldOut() {
+        return IsSoldOut;
+    }
+
+    public void setIsSoldOut(boolean isSoldOut) {
+        IsSoldOut = isSoldOut;
+    }
+
+    public Date getSoldOutDate() {
+        return SoldOutDate;
+    }
+
+    public void setSoldOutDate(Date soldOutDate) {
+        SoldOutDate = soldOutDate;
+    }
+
     @Override
     public Object getProperty(int index) {
         switch (index) {
@@ -36,6 +64,12 @@ public class Product extends BaseModel {
                 return ProductID;
             case 1:
                 return ProductName;
+            case 2:
+                return UnitPrice;
+            case 3:
+                return IsSoldOut;
+            case 4:
+                return SoldOutDate;
             default:
                 return null;
         }
@@ -43,13 +77,12 @@ public class Product extends BaseModel {
 
     @Override
     public int getPropertyCount() {
-        return 2;
+        return 5;
     }
 
     @Override
     public void getPropertyInfo(int i, Hashtable hashtable, PropertyInfo propertyInfo) {
-        switch (i)
-        {
+        switch (i) {
             case 0:
                 propertyInfo.type = PropertyInfo.INTEGER_CLASS;
                 propertyInfo.name = "ProductID";
@@ -57,6 +90,18 @@ public class Product extends BaseModel {
             case 1:
                 propertyInfo.type = PropertyInfo.STRING_CLASS;
                 propertyInfo.name = "ProductName";
+                break;
+            case 2:
+                propertyInfo.type = Float.class;
+                propertyInfo.name = "UnitPrice";
+                break;
+            case 3:
+                propertyInfo.type = PropertyInfo.BOOLEAN_CLASS;
+                propertyInfo.name = "IsSoldOut";
+                break;
+            case 4:
+                propertyInfo.type = Date.class;
+                propertyInfo.name = "SoldOutDate";
                 break;
             default:
                 break;
@@ -71,6 +116,15 @@ public class Product extends BaseModel {
                 break;
             case 1:
                 ProductName = (String) value;
+                break;
+            case 2:
+                UnitPrice = (Float) value;;
+                break;
+            case 3:
+                IsSoldOut = (Boolean) value;
+                break;
+            case 4:
+                SoldOutDate = (Date) value;
                 break;
             default:
                 return;
